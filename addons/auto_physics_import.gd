a6576e8445bdc40bc6e65b86a96e0f67c6f19398
ColdRiver <17830831606@163.com>
2025-11-07 01:31:49 +0800

    Init Repo

---
+++ addons/auto_physics_import.gd
@@ -0,0 +1,26 @@
+# This script is used to re-import gltf assets to make rigidbody objects with custom collosion meshes.
+# It also saves the rigidbody with collisions as a *.tscn in res://
+# 
+# Your gltf should have exactly one mesh with multiple children
+# The root mesh is used for the MeshInstance3D of the RigidBody3D
+# the child meshes are turned into CollisionShape3Ds for the RigidBody3D
+#
+# To use:
+# 1) Import gltf normally into Godot 4
+# 2) Select the resource in the FileSystem pane
+# 3) Select the Import pane
+# 4) Scroll down and load this script as the "Import Script"
+# 5) Select re-import
+
+@tool # Needed so it runs in the editor.
+extends EditorScenePostImport
+
+func _post_import(scene):
+	var traverse_base = scene.get_child(0).get_child(0).get_child(0)
+	var l = []
+	for t1 in traverse_base.get_children():
+		for t2 in t1.get_children():
+			#push_warning("{{%s}}" % scene.get_path_to(t2))
+			pass
+	return scene
+	
---
+++ player/player.gd
@@ -0,0 +1,58 @@
+extends CharacterBody3D
+
+const ORIGINAL_SPEED = 5
+const SPRINT_SPEED = 8
+var speed = ORIGINAL_SPEED
+const JUMP_VELOCITY = 9
+
+@onready var player_camera: Camera3D = $player_camera
+
+var camera_rotation_x: float = 0.0
+var camera_rotation_y: float = 0.0
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	camera_rotation_x = player_camera.rotation.x
+	camera_rotation_y = rotation.y
+
+func _process(_delta):
+	if Input.is_action_just_pressed("flash_light"):
+		$flash_light.visible = !$flash_light.visible
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		var mouse_delta = event.relative
+		camera_rotation_y -= mouse_delta.x * 0.005 # 控制角色左右转身
+		camera_rotation_x -= mouse_delta.y * 0.005 # 控制相机上下俯仰
+		camera_rotation_x = clamp(camera_rotation_x, -0.6, 0.2) # 根据需要调整角度
+		rotation.y = camera_rotation_y
+		player_camera.rotation.x = camera_rotation_x
+	if Input.is_action_just_pressed("ui_cancel"): # "ui_cancel" 通常是 ESC 键
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE) # 让光标可见
+		get_tree().set_pause(true) # 可选：暂停游戏，进入菜单
+	if Input.is_action_just_pressed("sprint"):
+		speed = SPRINT_SPEED
+	if Input.is_action_just_released("sprint"):
+		speed = ORIGINAL_SPEED
+
+
+func player_base_move(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	var input_dir := Input.get_vector("ui_right","ui_left",  "ui_down","ui_up" )
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+	move_and_slide()
+
+func _physics_process(delta: float) -> void:
+	player_base_move(delta)
+
+func _exit_tree():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
